f6d1228f3d36614091da5be10fecabe50720fd94
Saurav Pant <sauravpant777@gmail.com>
2025-12-25 11:45:04 +0545

    refactor: update token generation logic

--- backend/src/utils/token.ts
+++ backend/src/utils/token.ts
@@ -1,9 +1,9 @@
 import jwt from "jsonwebtoken";
 
-export const generateAccessToken = async (_id: string, role: string) => {
-  return jwt.sign({ _id, role }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: "30m" });
+export const generateAccessToken = async (_id: string) => {
+  return jwt.sign({ _id }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: "30m" });
 };
 
-export const generateRefreshToken = async (_id: string, role: string) => {
-  return jwt.sign({ _id, role }, process.env.REFRESH_TOKEN_SECRET, { expiresIn: "2d" });
+export const generateRefreshToken = async (_id: string) => {
+  return jwt.sign({ _id }, process.env.REFRESH_TOKEN_SECRET, { expiresIn: "2d" });
 };
